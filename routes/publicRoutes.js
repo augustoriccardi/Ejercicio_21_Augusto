bb4ddbbb055b01ddc007c324b45a209d7da7f8df
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-05-05 14:42:40 -0300

    devolver json de la api

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const pagesController = require("../controllers/pagesController");
 
 router.get("/", pagesController.showHome);
-
+router.get("/api/articulos", pagesController.indexjson);
 module.exports = router;
