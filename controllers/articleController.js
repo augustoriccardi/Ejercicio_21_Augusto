5a4f4e00dfd09e7c8e80ab7f7b8553925901f046
IvanPintos <ProyectosIvanPintos@gmail.com>
2023-05-07 16:32:29 -0300

    Funciones de admin: Crear, editar y borrar Articulos

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -21,16 +21,36 @@ async function create(req, res) {
 async function store(req, res) {}
 
 // Show the form for editing the specified resource.
-async function edit(req, res) {}
+async function edit(req, res) {
+  const article = await Article.findByPk(req.params.id);
+  res.render("panel", { modal: "editar", article });
+}
 
 // Update the specified resource in storage.
 async function update(req, res) {
-  const article = await Article.findByPk(id, { include: Comment });
-  res.render("panel", { modal: "editar" });
+  await Article.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+  );
+  res.redirect(`/admin`);
 }
 
 // Remove the specified resource from storage.
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  await Article.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.redirect(`/admin`);
+}
 
 // Otros handlers...
 // ...
--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,7 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const articleController = require("../controllers/articleController");
 
-router.get("/admin/crear", articleController.create);
-router.get("/admin/editar", articleController.update);
 module.exports = router;
